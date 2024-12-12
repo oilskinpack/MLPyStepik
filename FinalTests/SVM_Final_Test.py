63f7966bd36556043051bbecd7beea3246c9b857
Nikita Khabarov <khabarov.n.e@gmail.com>
2024-12-12 19:11:53 +0500

    Sample finished

--- FinalTests/SVM_Final_Test.py
+++ FinalTests/SVM_Final_Test.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+from CrossValidation.Grid_Search import grid_model
 
 res = ''
 
@@ -91,6 +98,97 @@ corr_qual = df.corr(numeric_only=True)['quality_num'].sort_values(ascending=True
 # sns.clustermap(df.corr(numeric_only=True),cmap='viridis')
 
 #endregion
+
+#endregion
+
+#region Создание dummy переменных и деление признака с целевой переменной
+
+y = df['quality']
+df = df.drop(columns=['quality_num','quality'],axis=1)
+X = pd.get_dummies(df,drop_first=True,dtype=int)
+res = y
+
+
+
+#endregion
+#region Деление данных
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=101)
+
+#endregion
+#region Создание звеньев для Pipeline
+
+scaler = StandardScaler()
+est = SVC(class_weight='balanced')
+
+#endregion
+#region Создание Pipeline
+
+operations = [('scaler',scaler),('est',est)]
+pipe = Pipeline(steps=operations)
+
+#endregion
+#region Создание GridSearchCV
+
+# c = [0.15,0.3,0.45,0.6,0.75,0.9,1.05]
+# gamma = ['scale','auto']
+
+c = [1.05]
+gamma = ['scale']
+param_grid = {'est__C':c
+              ,'est__gamma':gamma}
+grid_model = GridSearchCV(estimator=pipe
+                          ,param_grid=param_grid
+                          ,return_train_score=True
+                          ,scoring='f1_weighted'
+                          ,verbose=2
+                          ,cv=10)
+
+
+
+
+#endregion
+#region Обучение модели и предсказание
+
+grid_model.fit(X_train,y_train)
+y_pred = grid_model.predict(X_test)
+
+
+#Лучшие параметры - k = С = 1.05 gamma = scale
+# {'memory': None, 'steps': [('scaler', StandardScaler()), ('est', SVC(C=1.05, class_weight='balanced'))],
+#  'verbose': False, 'scaler': StandardScaler(), 'est': SVC(C=1.05, class_weight='balanced'),
+#  'scaler__copy': True, 'scaler__with_mean': True, 'scaler__with_std': True, 'est__C': 1.05,
+#  'est__break_ties': False, 'est__cache_size': 200, 'est__class_weight': 'balanced',
+#  'est__coef0': 0.0, 'est__decision_function_shape': 'ovr', 'est__degree': 3,
+#  'est__gamma': 'scale', 'est__kernel': 'rbf', 'est__max_iter': -1,
+#  'est__probability': False, 'est__random_state': None,
+#  'est__shrinking': True, 'est__tol': 0.001, 'est__verbose': False}
+res = grid_model.best_estimator_.get_params()
+
+
+#endregion
+
+#region Оценка
+#Матрица ошибок
+# [[ 17  10]
+#  [ 91 532]]
+confM = confusion_matrix(y_test,y_pred)
+res = confM
+
+#Визуализация матрицы ошибок
+ConfusionMatrixDisplay(confusion_matrix(y_pred, y_test)).plot()
+
+#Отчет по метрикам
+#               precision    recall  f1-score   support
+#
+#        Fraud       0.16      0.63      0.25        27
+#        Legit       0.98      0.85      0.91       623
+#
+#     accuracy                           0.84       650
+#    macro avg       0.57      0.74      0.58       650
+# weighted avg       0.95      0.84      0.89       650
+metricsRep = classification_report(y_test,y_pred)
+print(metricsRep)
 
 #endregion
 
